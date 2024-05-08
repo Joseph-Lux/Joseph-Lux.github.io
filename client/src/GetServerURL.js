7136ee407027ee44035005b3400473264785ab41
Joseph Lux <josephandrewlux@gmail.com>
2024-05-08 14:04:45 -0500

    Updated server url environment variable.

--- client/src/GetServerURL.js
+++ client/src/GetServerURL.js
@@ -1,3 +1,4 @@
-const serverURL = process.env.AWS_SERVER_BASE_URL || "http://localhost:5000";
+const serverURL =
+  process.env.REACT_APP_AWS_SERVER_BASE_URL || "http://localhost:5000";
 
 export default serverURL;
