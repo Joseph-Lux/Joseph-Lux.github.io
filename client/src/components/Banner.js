e6a859a6607bb85770f2aed5fe3bfb21e45e4341
Joseph Lux <josephandrewlux@gmail.com>
2024-05-24 16:45:13 -0500

    Remove unnecessary useNavigate from Banner

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useCart } from "./CartContext";
 import { Link } from "react-router-dom";
 
@@ -7,7 +7,6 @@ function Banner() {
   const [menuOpen, setMenuOpen] = useState(false); // State to track if menu is open or closed
   const { cartCount } = useCart();
   const currentPage = useLocation().pathname;
-  const navigate = useNavigate();
 
   const pages = ["Home", "About", "Gallery", "Store", "Blog"];
   const links = {
